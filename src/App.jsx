9ba2c6b3989b2d1e6c34a556c1d500aea290ddfd
monzurul002 <monzurul002@gmail.com>
2025-06-15 21:26:00 +0600

    blogs components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Blogs from "./components/Blogs/Blogs";
+import Bookmarks from "./components/Bookmarks/Bookmarks";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
-      <Blogs />
+      <main className="md:flex">
+        <Blogs />
+        <Bookmarks />
+      </main>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import profile from "../../assets/profile.png";
 const Header = () => {
   return (
-    <div>
-      <h1 className="font-bold text-xl">Knowlege Cafe</h1>
-    </div>
+    <header className="flex justify-between p-2 mx-4 border-b-2">
+      <h1 className="font-bold text-2xl ">Knowlege Cafe</h1>
+      <img src={profile} alt="" />
+    </header>
   );
 };
 
